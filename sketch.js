411d72f679544f8bfad6bed6f6cb4fbcc448ca01
MICHAEL STANDARSKI <michael.standarski@libertymutual.com>
2017-12-05 14:59:30 -0500

    opacity and white/green effect added

--- sketch.js
+++ sketch.js
@@ -5,10 +5,9 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0);
 	var x = 0;
-	var y = 0;
 	for (var i = 0; i <= width / symbolSize; i++) {
 		var stream = new Stream();
-		stream.generateSymbols(x, y);
+		stream.generateSymbols(x, random(-1000, 0));
 		streams.push(stream);
 		x += symbolSize;
 	}
@@ -16,19 +15,21 @@ function setup() {
 }
 
 function draw() {
-	background(0);
+	//the second value in background sets the opacity
+	background(0, 150);
 	streams.forEach(function(stream) {
 		stream.render();
 	})
 
 }
 
-function Symbol(x, y, speed) {
+function Symbol(x, y, speed, first) {
 	this.x = x;
 	this.y = y;
 	this.value;
 	this.speed = speed;
 	this.switchInterval = round(random(2, 20));
+	this.first = first;
 
 	this.setToRandomSymbol = function() {
 		if (frameCount % this.switchInterval == 0) {
@@ -53,19 +54,26 @@ function Symbol(x, y, speed) {
 function Stream() {
 	this.symbols = [];
 	this.totalSymbols = round(random(5, 30));
-	this.speed = random(5, 20);
+	this.speed = random(1, 3);
 
 	this.generateSymbols = function(x, y) {
+		var first = round(random(0, 4)) == 1;
+		
 		for (var i = 0; i <= this.totalSymbols; i++) {
-			symbol = new Symbol(x, y, this.speed);
+			symbol = new Symbol(x, y, this.speed, first);
 			symbol.setToRandomSymbol();
 			this.symbols.push(symbol);
-			y -= symbolSize;	
+			y -= symbolSize;
+			first = false;
 		}
 	}
 	this.render = function() {
 		this.symbols.forEach(function(symbol) {
+			if (symbol.first) {
+				fill(180, 255, 180);
+			} else {
 			fill(0, 255, 70);
+			}
 			text(symbol.value, symbol.x, symbol.y);
 			symbol.rain();
 			symbol.setToRandomSymbol();
